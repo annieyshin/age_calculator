4fb15799b6ff62e9fc05698ff9798a3b24c82892
Annie Shin <annieyshin@gmail.com>
2018-08-03 18:59:20 -0700

    add in more HTML

--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Birthdate } from './birthdate.js';
+
+
+$(document).ready(function() {
+  $('#age-form').submit(function(event) {
+    event.preventDefault();
+    let birthdate = $('#birthdate');
+    let output = ageYearConvert(birthdate);
+    output.forEach(function(element) {
+      $('#earthYears').append("<li>" + element + "</li>");
+    });
+  });
+});
